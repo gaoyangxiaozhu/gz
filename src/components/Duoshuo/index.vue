89efe1396b34075077ff8fb79f17a3ec6015b59d
gaoyangxiaozhu <1456543560@qq.com>
2016-11-03 22:25:27 +0800

    re-build

--- src/components/Duoshuo/index.vue
+++ src/components/Duoshuo/index.vue
@@ -20,18 +20,20 @@ export default {
             short_name: this.shortName
         }
         /* eslint-disable */
-        loadScript('lib/duoshuo/index.js')
+        loadScript('http://static.duoshuo.com/embed.js')
         .then(()=>{
             let url = this.url || window.location
             let thread = this.thread
-            console.log(url, thread)
             let container = this.$el
-            let el = document.createElement('div')
-            el.setAttribute('data-thread-key', thread)
-            el.setAttribute('data-url', url)
-            container.innerHTML = ''
-            DUOSHUO.EmbedThread(el)
-            container.appendChild(el)
+            
+            setTimeout(()=>{
+                let el = document.createElement('div')
+                el.setAttribute('data-thread-key', thread)
+                el.setAttribute('data-url', url)
+                container.innerHTML = ''
+                DUOSHUO.EmbedThread(el)
+                container.appendChild(el)
+            }, 1000)
         })
         .catch((e) => {
             console.log(e)
