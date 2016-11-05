57049d1e77cb81475a4a20b5a514e20cac10c654
gaoyangxiaozhu <1456543560@qq.com>
2016-11-05 09:58:32 +0800

    re-build

--- src/components/App.vue
+++ src/components/App.vue
@@ -288,6 +288,9 @@ export default {
     top: 0;
     width: 100%;
     height: 100%;
+
+    overflow-x: hidden;
+    transform: translate3d(0, 0, 0);
     background-color: #000;
     opacity: 0;
 
